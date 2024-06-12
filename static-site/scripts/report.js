10226185232a238ad7d39003369aa9b6860fb9fa
Leo Skadden <leo@skadden.tech>
2024-06-12 09:39:59 -0400

    add search to the map

--- static-site/scripts/report.js
+++ static-site/scripts/report.js
@@ -2,6 +2,11 @@ const MRC = [28.059642, -80.590976];
 
 function main() {
 	const map = L.map('map').setView(MRC, 13);
+	const search = new GeoSearch.GeoSearchControl({
+		provider: new GeoSearch.OpenStreetMapProvider(),
+	});
+
+	map.addControl(search);
 
 	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		maxZoom: 19,
